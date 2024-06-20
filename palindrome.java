ad56790c4c61e0d3a357085b0e14ba6695d6148d
Vishwasbm05 <vishwasbm2000@gmail.com>
2024-06-20 14:07:56 +0530

    Plaindrome Program

--- palindrome.java
+++ palindrome.java
@@ -2,4 +2,25 @@ package LocolDemo;
 
 public class palindrome {
     
+        static int reverseNumber(int n){
+            int reverseNo=0;
+            while(n>0){
+                reverseNo=reverseNo*10+n%10;
+                n=n/10;
+            }
+            return reverseNo;
+        }
+    
+    public static void main(String[] args){
+        int n=123455;
+        int reverseN=reverseNumber(n);
+        System.out.println("Reverse of n = "+reverseN);
+
+        if(n==reverseN){
+            System.out.println("Palindrome=yes");
+
+        }
+        else
+        System.out.println("palindrome=no");
+    }
 }
